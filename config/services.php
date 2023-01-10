d8dd06019e23883c8acbd927cb41a22782f6a15d
viryl scott <viryl1515@gmail.com>
2023-01-10 09:11:31 +0100

    wip

--- config/services.php
+++ config/services.php
@@ -39,7 +39,7 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => env('GITHUB_REDIRECT_URL'),
+        'redirect' => env('GOOGLE_REDIRECT_URL'),
     ],
     'twitter' => [
         'client_id' => env('TWITTER_CLIENT_ID'),
