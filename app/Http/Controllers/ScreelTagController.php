ee9afc7685fda2f96d44116e20f49166c3e6ed1b
viryl scott <viryl1515@gmail.com>
2023-01-15 13:58:46 +0100

    wip, get user screels

---
+++ app/Http/Controllers/ScreelTagController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ScreelTag;
+use Illuminate\Http\Request;
+
+class ScreelTagController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ScreelTag  $screelTag
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ScreelTag $screelTag)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\ScreelTag  $screelTag
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ScreelTag $screelTag)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ScreelTag  $screelTag
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ScreelTag $screelTag)
+    {
+        //
+    }
+}
